17c760d15b234954b1754d9d08909bf94ef48c5e
ssivanatarajan <ssivanatarajan@gmail.com>
2017-04-22 16:32:38 +0530

    removed flash lib

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var logger = require('morgan')
 var websockets = require('./websockets')
 var bodyParser = require('body-parser')
-var flash 			= require('express-flash-notification')
+//var flash 			= require('express-flash-notification')
 var cookieParser	= require('cookie-parser')
 var session			= require('express-session')
 var app = express()
@@ -15,7 +15,7 @@ app.use(session({secret: 'a4f8071f-c873-4447-8ee2',
    cookie: { maxAge: 2628000000 },
 	resave: true, 
 	saveUninitialized: true}))
-app.use(flash())
+//app.use(flash())
 
 var port = process.env.PORT || 3000
 var server = app.listen(port, function () {
